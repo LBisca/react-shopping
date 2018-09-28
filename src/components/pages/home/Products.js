4210a36add0b41b42a51be08fe5a48d93a1f9cff
LBisca <lucas.simon.b@gmail.com>
2018-09-28 14:40:54 +0000

    Remove Logic from inside the render

--- src/components/pages/home/Products.js
+++ src/components/pages/home/Products.js
@@ -5,6 +5,23 @@ import "./scss/products.css";
 import React from "react";
 
 const Products = props => {
+  const products = props.value.products.map((item, index) => {
+    if (!props.category) {
+      return <Product key={index} item={item} />;
+    }
+    if (props.category === "Men") {
+      if (item.category === "Men" || item.category === "Diverse") {
+        return <Product key={index} item={item} />;
+      }
+    }
+    if (props.category === "Women") {
+      if (item.category === "Women" || item.category === "Diverse") {
+        return <Product key={index} item={item} />;
+      }
+    }
+    return null;
+  });
+
   return (
     <Consumer>
       {value => {
@@ -13,30 +30,7 @@ const Products = props => {
             <Banner />
             <div className="products">
               <h1 className="products--title">Latest Products</h1>
-              <div className="products--container">
-                {value.products.map((item, index) => {
-                  if (!props.category) {
-                    return <Product key={index} item={item} />;
-                  }
-                  if (props.category === "Men") {
-                    if (
-                      item.category === "Men" ||
-                      item.category === "Diverse"
-                    ) {
-                      return <Product key={index} item={item} />;
-                    }
-                  }
-                  if (props.category === "Women") {
-                    if (
-                      item.category === "Women" ||
-                      item.category === "Diverse"
-                    ) {
-                      return <Product key={index} item={item} />;
-                    }
-                  }
-                  return null;
-                })}
-              </div>
+              <div className="products--container">{products}</div>
             </div>
           </React.Fragment>
         );
@@ -45,4 +39,10 @@ const Products = props => {
   );
 };
 
-export default Products;
+export default props => (
+  <Consumer>
+    {value => {
+      return <Products {...props} value={value} />;
+    }}
+  </Consumer>
+);
